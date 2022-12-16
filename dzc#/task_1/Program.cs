be984784d0a19a8dc0dba48899d33d71fa522f7c
Euphoria <polunine28@gmail.com>
2022-12-16 14:46:10 +1100

    task1

---
+++ dzc#/task_1/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 1. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+
+// m = 3, n = 4.
+
+// 0,5 7 -2 -0,2
+
+// 1 -3,3 8 -9,9
+
+// 8 7,8 -7,1 9
+
+double[,] a = new double[5, 10];
+
+Random random = new Random();
+System.Console.WriteLine("Двумерный массив размером mXn > ");
+for (int i = 0; i < 5; i++)
+{
+    for (int j = 0; j < 10; j++)
+    {
+        a[i, j] = random.Next(100);
+        Console.Write("{0,4}", a[i, j]);
+    }
+    Console.WriteLine();
+}
